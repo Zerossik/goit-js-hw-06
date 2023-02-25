89b2c5e120ed8cba8e2d63a32ecc959d09e7fecc
Zerossik <zeross19940303@gmail.com>
2023-02-26 01:00:51 +0200

    task - 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsEl = document.querySelector('#ingredients');
+const colectionEl = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = ingredients[i];
+  ingredient.classList.add('item');
+  colectionEl.push(ingredient);
+}
+ingredientsEl.prepend(...colectionEl);
