fd5267df95d312c06c318613e5ec07cc3482c70a
Zerossik <zeross19940303@gmail.com>
2023-02-28 21:02:39 +0200

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+function handleChangeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+btnEl.addEventListener('click', handleChangeColor);
