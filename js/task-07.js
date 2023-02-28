a8f3bc7208226219193a5924967235db4314f047
Zerossik <zeross19940303@gmail.com>
2023-02-28 19:35:45 +0200

    task-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,5 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
-console.log(typeof inputEl.value);
 
 function handleChangeInput() {
   spanEl.style.fontSize = `${inputEl.value}px`;
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputEl = document.querySelector('.login-form');
 
+function handleValidationForm(event) {
+  event.preventDefault();
+  if (inputEl.elements.email.value === '' || inputEl.elements.password.value === '') {
+    alert('Заповніть всі поля!');
+    return;
+  }
+
+  const userData = {
+    email: inputEl.elements.email.value,
+    password: inputEl.elements.password.value,
+  };
+
+  console.log(userData);
+  inputEl.reset();
+}
+inputEl.addEventListener('submit', handleValidationForm);
